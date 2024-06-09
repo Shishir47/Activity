9d9bb7f9ade3f12d928a8cbb97bc078f5587eac3
Raufun Nasim Khan Chowdhury <madmaxshishir47@gmail.com>
2024-06-10 05:25:13 +0600

    All Activity life cycle added!

--- app/src/main/java/com/shishir/activity/MainActivity.kt
+++ app/src/main/java/com/shishir/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.shishir.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,27 +16,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-
         }
+        Toast.makeText(this@MainActivity, "On Created is Called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStart() {
         super.onStart()
+        Toast.makeText(this@MainActivity,"On Start is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onResume() {
         super.onResume()
+        Toast.makeText(this@MainActivity,"On Resume is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onPause() {
         super.onPause()
+        Toast.makeText(this@MainActivity,"On Pause is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onStop() {
         super.onStop()
+        Toast.makeText(this@MainActivity,"On Stop is called", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Toast.makeText(this@MainActivity,"On Destroy is called", Toast.LENGTH_SHORT).show()
     }
 }
